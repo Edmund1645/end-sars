18d492f8e35da4c33f387ceab418b4e13a92aebb
edmund1645 <edmund.timfon@gmail.com>
2020-10-05 17:54:27 +0100

    fix undefined

--- crons/schedules.js
+++ crons/schedules.js
@@ -1,7 +1,7 @@
 const T = require('../lib/twit');
 new Date().toLocaleDateString();
 const hashtags = '#EndSarsNow #EndSARS';
-const handles = '@PoliceNG @MBuhari @NigeriaGov';
+const handles = '@PoliceNG @MBuhari @Laurestar @NigeriaGov';
 const messages = [
   `Today is ${new Date().toLocaleDateString()} and the government has not taken any reasonable action to end the brutality of the Nigerian Police Force.`,
   'Banning SARS is not enough, the entire Police Force need a refresher on their duty.',
@@ -12,7 +12,7 @@ T.post(
   'statuses/update',
   {
     status:
-      messages[Math.round(Math.random() * messages.length)] +
+      messages[Math.floor(Math.random() * messages.length)] +
       ` 
     ${handles} 
     ${hashtags}
