1bb0e44d6f2487cd778937edc271adab99275577
edmund1645 <edmund.timfon@gmail.com>
2020-10-20 09:59:55 +0100

    update messages

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
 require('dotenv').config();
 
 const T = require('./lib/twit');
+
+const stream = T.stream('statuses/filter', {
+  track: ['#EndSars', '#EndSarsNow'],
+  lang: 'en',
+});
+
+const shouldRetweet = () => {
+  if (Math.random() >= 0.02) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const shouldLike = () => {
+  if (Math.random() >= 0.02) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const errorHandler = (err, response, data) => {
+  console.log(err);
+};
+
+// stream.on('tweet', (tweet) => {
+//   // retweet and like if has not been done already
+//   if (tweet.favorited === false && shouldLike()) {
+//     T.post('favorites/create', { id: tweet.id_str }, errorHandler);
+//     console.log('liked');
+//   }
+//   if (tweet.retweeted === false && shouldRetweet()) {
+//     T.post('statuses/retweet/:id', { id: tweet.id_str }, errorHandler);
+//     console.log('retweeted');
+//   }
+//   return;
+// });
--- crons/schedules.js
+++ crons/schedules.js
@@ -1,11 +1,15 @@
 const T = require('../lib/twit');
-new Date().toLocaleDateString();
 const hashtags = '#EndSarsNow #EndSARS';
-const handles = '@PoliceNG @MBuhari @Laurestar @NigeriaGov';
+const handles =
+  '@PoliceNG @MBuhari @NigeriaGov @ProfOsinbajo @femigbaja @DrAhmadLawan';
 const messages = [
   `Today is ${new Date().toLocaleDateString()} and the government has not taken any reasonable action to end the brutality of the Nigerian Police Force.`,
-  'Banning SARS is not enough, the entire Police Force need a refresher on their duty.',
-  'How is this country supposed to be any better when the next generation is being wiped out one by one?',
+  'Banning SARS is not enough, the entire Police Force need a refresher on their duty. AND WE DO NOT WANT SWAT!!',
+  'We do not want rebranded SARS a.k.a SWAT',
+  'What do we want???? \n END SARS',
+  'Lives are being lost daily because the Nigerian government would rather spend time doing the wrong thing than the right thing',
+  'We will not stop until we see results',
+  'Today is as good as any other day to do the right thing',
 ];
 
 T.post(
